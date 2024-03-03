3264f1c828f861feed166a2fdf000215adc4ffc1
Harshana Jayasekara <98016320+HarshanaJayasekara@users.noreply.github.com>
2024-03-04 00:17:28 +0530

    Array Object
    Array Object

--- ii.php
+++ ii.php
@@ -10,7 +10,15 @@
     <?php
         echo "hello";
         $name = "harhans";
+        $int = 25;
+        $float =2.2;
+        $bool = true;
+
+        $age = [2,5,8];
+
+        $object = new obj();
         echo $name ;
+
 
     ?>
     
